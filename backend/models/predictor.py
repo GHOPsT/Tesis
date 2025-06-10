410f33fb546a103a06c5700f149e7576aeb307fb
GHOPsT <mggsalvator@gmail.com>
2025-06-10 18:42:37 -0500

    añadido el modeloRFv4

--- backend/models/predictor.py
+++ backend/models/predictor.py
@@ -1,41 +1,18 @@
 from joblib import load
 import pandas as pd
+import numpy as np
 
-# Carga el modelo y los codificadores de etiquetas guardados
-model = load('./ml/modeloRFv2/modelo_rf_profesionales_simple.joblib')
-le_centro = load('./ml/modeloRFv2/le_ipress_simple.joblib')
-le_esp = load('./ml/modeloRFv2/le_especialidad_simple.joblib')
+# Carga los modelos y encoders
+model_att = load("modeloRFv3/modelo_rf_atenciones.joblib")
+model_prof = load("modeloRFv3/modelo_rf_profesionales_cascada.joblib")
+le_ipress = load("modeloRFv3/le_ipress_simple.joblib")
+le_especialidad = load("modeloRFv3/le_especialidad_simple.joblib")
 
-def predict_specialists(year, month, centro_salud, especialidad):
-    print("Especialidaddes reconocidas:", le_esp.classes_)
-    print("Centros de salud reconocidos:", le_centro.classes_)
-    """
-    Predice la cantidad de especialistas y especialidades necesarias para un centro de salud,
-    dado el año, mes y centro de salud.
-    """
-    # Validar que los valores existen en los encoders
-    if centro_salud not in le_centro.classes_:
-        return {'error': f'Centro de salud desconocido: {centro_salud}'}
-    if especialidad not in le_esp.classes_:
-        return {'error': f'Especialidad desconocida: {especialidad}'}
-
-    # Codifica el centro de salud usando el label encoder
-    centro_encoded = le_centro.transform([centro_salud])[0]
-    especialidad_encoded = le_esp.transform([especialidad])[0]
-    
-    # Prepara los datos de entrada para el modelo
-    input_df = pd.DataFrame([{
-        'COD_IPRESS': centro_encoded,
-        'DESC_SERVICIO': especialidad_encoded,
-        'MES': month,
-        'AÑO': year
-    }])
-    
-    # Realiza la predicción
-    prediction = model.predict(input_df)
-    
-    # Si el modelo predice dos valores (especialistas y especialidades)
-    # puedes devolver ambos, por ejemplo:
-    # return {'especialistas': prediction[0][0], 'especialidades': prediction[0][1]}
-    # Si solo predice uno, ajusta según corresponda:
-    return {'prediction': int(prediction[0])}
+def predecir_profesionales(anio, mes, ipress, especialidad):
+    cod_ipress = le_ipress.transform([ipress])[0]
+    cod_esp = le_especialidad.transform([especialidad])[0]
+    X_att = np.array([[anio, mes, cod_ipress, cod_esp]])
+    pred_atenciones = model_att.predict(X_att)[0]
+    X_prof = np.array([[anio, mes, cod_ipress, cod_esp, pred_atenciones]])
+    pred_prof = model_prof.predict(X_prof)[0]
+    return round(pred_prof)
